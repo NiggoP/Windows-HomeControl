9638b9dfe2e2c1e0b37076c16582bc10154cd3bc
NiggoP <nicholas.poggendorf@hotmail.com>
2021-05-09 23:32:12 +0200

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Windows-HomeControl
+
+## Installation
+
+git clone <repo>
+
+cd <repo>
+
+python -m vitualenv venv
+
+.\venv\Scripts\activate
+
+pip install -r requirements.txt
+
+##Usage
+
+py main.py
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import platform
+import time
 import PySimpleGUIQt as sg
 import paho.mqtt.client as mqtt
 from playsound import playsound
@@ -34,6 +35,9 @@ def on_message(client, userdata, message):
     if pl == "bellExt":
         print("RING RING")
         notify()
+        if autoOpen:
+            client.publish("/home/doorbell","openDoor")#publish
+
     elif pl == "bellInt":
         print("RING RING Drin")
         notify()
@@ -41,6 +45,8 @@ def on_message(client, userdata, message):
 
 def main():
     global alarm
+    global autoOpen
+    timeout = 0
     h = platform.uname()[1]
     broker_address="192.168.178.54"
     client = mqtt.Client(str(h))
@@ -49,7 +55,7 @@ def main():
     client.connect(broker_address)
     client.loop_start()
     #openDoor(client)
-    menu_def = ['BLANK', ['&Open Door', '&Auto open', '---', '&Close']]
+    menu_def = ['BLANK', ['&Open Door', '&Activate Auto-Open', '---', '&Close']]
 
     tray = sg.SystemTray(menu=menu_def)
     #tray.ShowMessage("RING", "The doorbell rung", filename=None, data=None, data_base64=None, messageicon=None, time=5000)
@@ -60,16 +66,45 @@ def main():
         if menu_item == 'Close':
             break
         elif menu_item == 'Open Door':
+            print(timeout)
             client.publish("/home/doorbell","openDoor")#publish
-        elif menu_item == 'Auto open':
-            autoOpen = True
+        elif menu_item == 'Deactivate Auto-Open':
+            autoOpen = False
+            timeout = 0
+            sg.popup('Feature was deactivated!')
+            menu_def = ['BLANK', ['&Open Door', '&Activate Auto-Open', '---', '&Close']]
+            tray.Update(menu=menu_def)
+        elif menu_item == 'Activate Auto-Open':
+            input = sg.popup_get_text('Please input how long this feature should be activated in minutes (max. 60)', 'Automated Door Opening', '10', size = (50, 30))
+            if input == None:
+                sg.popup('Feature was not activated!')  # Shows OK button
+            elif int(input) < 1 or int(input) > 60:
+                sg.popup('Not a valid value!')  # Shows OK button
+            else:
+                autoOpen = True
+                menu_def = ['BLANK', ['&Open Door', '&Deactivate Auto-Open', '---', '&Close']]
+                tray.Update(menu=menu_def)
+                timeout = time.time() + int(input)*60
+
 
 
         if alarm:
             print("alarm")
-            playsound('alarm.mp3')
+            if autoOpen:
+                autoOpen = False
+                tray.ShowMessage("RING - Opening", "The door has been opened automatically", filename=None, data=None, data_base64=None, messageicon=None, time=5000)
+            else:
+                playsound('alarm.mp3')
+                tray.ShowMessage("RING", "The doorbell just rang", filename=None, data=None, data_base64=None, messageicon=SYSTEM_TRAY_MESSAGE_ICON_INFORMATION, time=5000)
             alarm = False
-            tray.ShowMessage("RING", "The doorbell just rang", filename=None, data=None, data_base64=None, messageicon=None, time=5000)
+
+        #Update autoOpen
+        if time.time() > timeout and timeout != 0:
+            print("Timeout reached -- Disabling autoOpen")
+            autoOpen = False
+            timeout = 0
+            menu_def = ['BLANK', ['&Open Door', '&Activate Auto-Open', '---', '&Close']]
+            tray.Update(menu=menu_def)
 
 if __name__ == "__main__":
     main()
